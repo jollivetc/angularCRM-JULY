e8f0ef2114001c2e17fed53bc1ba23c84e15f767
Christophe Jollivet <jollivet@apside.fr>
2022-07-20 12:00:16 +0200

    reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,23 +8,5 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'angularCRM';
-  fruits = ['apple','banana', 'pear','cherry'];
-  person = {
-    firstname:'Bob',
-    lastname:'Morane',
-    age:33
-  }
-  cssClass = 'red';
-  email='myemail.com'
-
-  clicked($event:any):void{
-    console.log($event)
-    this.title='see you tomorrow';
-  }
-
-  registerUser(signupForm :NgForm){
-    console.log(`the email is ${this.email}`)
-    console.log(signupForm.value)
-    this.email = "toto";
-  }
+  
 }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'crm-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm : FormGroup;
+  constructor() {
+    this.loginForm = new FormGroup({
+      login: new FormControl('',[Validators.required, Validators.minLength(3)]),
+      password : new FormControl('',[Validators.required, checkPassword])
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit():void{
+    console.log(this.loginForm)
+  }
+}
+
+function checkPassword(c:AbstractControl): ValidationErrors | null{
+  if((c.value as string).indexOf('$') < 0){
+    return null
+  }else{
+    return { checkPassword: 'there is a $'}
+  }
+}
+
