61dc03c5eb55e5ba1b5381addaf4a14af55eecbf
Christophe Jollivet <jollivet@apside.fr>
2022-07-22 10:53:57 +0200

    beginning of consumer list

---
+++ src/app/consumer/consumer-list/consumer-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'crm-consumer-list',
+  templateUrl: './consumer-list.component.html',
+  styleUrls: ['./consumer-list.component.scss']
+})
+export class ConsumerListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
