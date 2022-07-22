bcb82d3af835fb7777456a156b6019a3fbedafe3
Christophe Jollivet <jollivet@apside.fr>
2022-07-22 14:04:15 +0200

    creation consumer-form

---
+++ src/app/consumer/consumer-form/consumer-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'crm-consumer-form',
+  templateUrl: './consumer-form.component.html',
+  styleUrls: ['./consumer-form.component.scss']
+})
+export class ConsumerFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
